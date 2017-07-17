22a8e297c147be7de7da9545f596efec5d43a108
Anton <organatik@gmail.com>
2017-07-17 14:18:18 +0300

    fix

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -13,19 +13,22 @@ import {
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
-  // myForm: FormGroup;
-  // email: AbstractControl;
-  // password: AbstractControl;
+  myForm: FormGroup;
+  email: AbstractControl;
+  password: AbstractControl;
 
 
-  // constructor(fb: FormBuilder) {
-  //   this.myForm = fb.group({
-  //     'email': ['', Validators.compose([
-  //       Validators.required, myValidations])],
-  //     'password': ['', Validators.compose([
-  //       Validators.required, myValidations])]
-  //   });
-  // }
+  constructor(fb: FormBuilder) {
+    this.myForm = fb.group({
+      'email': ['', Validators.compose([
+        Validators.required, ])],
+      'password': ['', Validators.compose([
+        Validators.required, ])]
+    });
+    this.email = this.myForm.controls['email'];
+    this.password = this.myForm.controls['password'];
+  }
+  onSubvit
 
   ngOnInit() {
   }
